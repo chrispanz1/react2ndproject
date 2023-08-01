8e7d60167354c204f0ebcde50f6a3b30c3ef695f
chrispanz1 <133619340+chrispanz1@users.noreply.github.com>
2023-08-01 14:56:11 -0400

    notes

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -5,6 +5,9 @@ function Page() {
     <div>
         <p> Welcome to Nation Schola!</p>
         <h3>Here we display information of the nations of the world!</h3>
+        <marquee behavior="scroll" direction="right">
+    <img src="https://img.freepik.com/free-vector/planet-earth_1308-82523.jpg?w=2000" width="120" height="80" alt="Natural" />
+  </marquee>
     </div>
   )
 }
